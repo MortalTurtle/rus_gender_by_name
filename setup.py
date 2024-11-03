6de09c92442a27b59ca1214e0d15b8e893808887
MortalTurtle <ars.lukin@yandex.ru>
2024-11-04 03:26:53 +0500

    new version hoorraay

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='gender_by_rus_name',
-    version='1.0.0',
+    version='0.0.2',
     packages=['genderbyrusname'],
     url='https://github.com/MortalTurtle/rus_gender_by_name/tree/master/rusgenderdetection',
     author='Nikolay Putko',
